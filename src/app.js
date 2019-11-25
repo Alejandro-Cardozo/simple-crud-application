5f21db14fabe130235b23c20d7ecfc9215d0291c
Alejandro Cardozo <uri.cardozo@gmail.com>
2019-11-25 18:54:48 -0300

    Backend setup + main views

---
+++ src/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
+const session = require("express-session");
+
+// Initializations
+const app = express();
+require("./database");
+
+// Settings
+app.set("port", process.env.PORT || 5000);
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs"
+  })
+);
+app.set("view engine", ".hbs");
+
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Global Variables
+
+// Routes
+app.use(require("./routes/index"));
+app.use(require("./routes/profile"));
+app.use(require("./routes/users"));
+
+// Static Files
+app.use(express.static(path.join(__dirname, "public")));
+
+module.exports = app;
