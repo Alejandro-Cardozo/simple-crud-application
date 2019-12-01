684b8fcce1dbe6a3ae5ac470220dff858ad2efbc
Alejandro Cardozo <uri.cardozo@gmail.com>
2019-12-01 17:25:39 -0300

    Navigation and distinction of users by sessions

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 
 const File = require("../models/File");
+const { isAuth } = require("../helpers/authentication");
 
-router.get("/profile/add", (req, res) => {
+router.get("/profile/add", isAuth, (req, res) => {
   res.render("files/new-file");
 });
 
-router.post("/profile/new-file", async (req, res) => {
+router.post("/profile/new-file", isAuth, async (req, res) => {
   const { title, description } = req.body;
   const errors = [];
   if (!title) {
@@ -23,30 +24,31 @@ router.post("/profile/new-file", async (req, res) => {
     });
   } else {
     const newFile = new File({ title, description });
+    newFile.user = req.user.id;
     await newFile.save();
     req.flash("success_msg", "File added successfully");
     res.redirect("/profile");
   }
 });
 
-router.get("/profile", async (req, res) => {
-  const files = await File.find().sort({ date: "desc" });
+router.get("/profile", isAuth, async (req, res) => {
+  const files = await File.find({ user: req.user.id }).sort({ date: "desc" });
   res.render("files/all-files", { files });
 });
 
-router.get("/profile/edit/:id", async (req, res) => {
+router.get("/profile/edit/:id", isAuth, async (req, res) => {
   const file = await File.findById(req.params.id);
   res.render("files/edit-file", { file });
 });
 
-router.put("/profile/edit-file/:id", async (req, res) => {
+router.put("/profile/edit-file/:id", isAuth, async (req, res) => {
   const { title, description } = req.body;
   await File.findByIdAndUpdate(req.params.id, { title, description });
   req.flash("success_msg", "File updated");
   res.redirect("/profile");
 });
 
-router.delete("/profile/delete/:id", async (req, res) => {
+router.delete("/profile/delete/:id", isAuth, async (req, res) => {
   await File.findByIdAndDelete(req.params.id);
   req.flash("success_msg", "File deleted");
   res.redirect("/profile");
